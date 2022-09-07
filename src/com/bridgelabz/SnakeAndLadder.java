f691e7aac4f58cf292f3eb563d1914a16bd5ce17
pavithramuddu93 <pavithramuddu93>
2022-09-07 19:06:47 +0530

    Welcome

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author PAVITHRA C
+ *
+ */
+public class SnakeAndLadder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Snake and Ladder Program");
+
+		int startPosition = 0;
+
+	}
+
+	}
+
+
