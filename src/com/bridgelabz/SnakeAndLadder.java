e1b7b461bef9ee98e29a7618bc74b9ac72215f40
pavithramuddu93 <pavithramuddu93>
2022-09-07 19:15:41 +0530

    UC-3-CheckForOption

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -12,19 +12,40 @@ public class SnakeAndLadder {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Snake and Ladder Program..");
+		int noPlay = 0;
+		int ladder = 1;
+		int snake = 2;
 
-		int startPosition = 0;
-		Random randomNum = new Random();
-		int dice = randomNum.nextInt(6) + 1;
-		System.out.println(dice);
+		public static void main(String[] args) {
+			System.out.println("Welcome to Snake and Ladder Program..");
+
+			int startPosition = 0;
+			Random randomNum = new Random();
+			int dice = randomNum.nextInt(6) + 1;
+			System.out.println("Dice Number:-" + dice);
+
+			int noPlay = 0;
+			int ladder = 1;
+			int snake = 2;
+
+			int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
+			System.out.println("Random Number:-" + checkPlay);
+
+			switch (checkPlay) {
+			case 0:
+				break;
+
+			case 1:
+				startPosition += dice;
+				break;
+
+			case 2:
+				startPosition -= dice;
+				break;
+			}
+
+			System.out.println("Start Position:-" + startPosition);
+
+		}
 
 	}
-
-}
-
-	
-
-
